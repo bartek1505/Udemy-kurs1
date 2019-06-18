bfa5805041566111ca72db30c5fba5589cfcdb04
Bartek <Bartek@LAPTOP-1H508GIL>
2019-06-18 17:14:27 +0200

    Drugi commit do mastera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @Component({
   selector: 'app-root',
